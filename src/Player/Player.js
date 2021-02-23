3453c219a8ebdc02ab4de63b77a78bb07ca2af1a
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-02-23 21:53:19 +0100

    Add styled-components logic to App.js

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -2,11 +2,13 @@ import styled from 'styled-components/macro'
 import Button from '../Button'
 
 export default function Player({ name, score, onMinus, onPlus }) {
+  const hue = Math.min(score * 10, 120)
+
   return (
     <>
       <PlayerWrapper>
-        <h2> {name} </h2>
-        <ScoreWrapper>
+        <span> {name} </span>
+        <ScoreWrapper color={`hsl(${hue}, 50%, 50%)`}>
           <CounterButton onClick={onMinus}>➖</CounterButton>
           <span> {score} </span>
           <CounterButton onClick={onPlus}>➕</CounterButton>
@@ -22,12 +24,15 @@ const PlayerWrapper = styled.section`
   justify-content: space-between;
 `
 const ScoreWrapper = styled.div`
+  color: ${props => props.color || 'black'};
   display: grid;
   gap: 5px;
   grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+  justify-items: center;
 `
 const CounterButton = styled(Button)`
-  background-color: palevioletred;
-  border: none;
+  background-color: darksalmon;
+  border: 1px solid black;
   color: white;
 `
